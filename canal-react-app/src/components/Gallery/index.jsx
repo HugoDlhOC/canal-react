943c00ef887aaaa81056a56aa9288293b8dae916
HugoDlhOC <hugo.delahaye53@outlook.fr>
2022-12-09 15:06:09 +0100

    add modal

--- canal-react-app/src/components/Gallery/index.jsx
+++ canal-react-app/src/components/Gallery/index.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faArrowRight,
+  faStar,
+  faClose,
+} from "@fortawesome/free-solid-svg-icons";
+import emptyImage from "../../data/images/empty_image.png";
 
 const Gallery = ({ urlApi, optionalParameterUrlApi, galleryTitle }) => {
   const [data, setData] = useState(undefined);
@@ -10,6 +16,8 @@ const Gallery = ({ urlApi, optionalParameterUrlApi, galleryTitle }) => {
   const [nextDistanceCount, setNextDistanceCount] = useState(0); //counter next elements
   const [directionType, setDirectionType] = useState(""); //define type of click : previous or next
   const [clickCount, setClickCount] = useState(0); //counter global for one click to previous and next elements
+  const [modal, setModal] = useState(false);
+  const [modalContent, setModalContent] = useState(undefined);
 
   //api key
   const API_KEY = "ade02ea5f909697eb890e5479757edf0";
@@ -56,6 +64,53 @@ const Gallery = ({ urlApi, optionalParameterUrlApi, galleryTitle }) => {
     }
   }, [previousDistanceCount, nextDistanceCount]); //impact for previousDistanceCount and nextDistanceCount
 
+  //modal
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const changeContentModal = (content) => {
+    let item = {
+      title: "",
+      description: "",
+      backgroundImage: "",
+      vote: "",
+    };
+    console.log(content);
+    if (content.media_type !== undefined) {
+      if (content.media_type === "movie") {
+        item.title = content.original_title;
+      }
+      if (content.media_type === "tv") {
+        item.title = content.name;
+      }
+    } else if (
+      content.media_type === undefined &&
+      content.original_title === undefined
+    ) {
+      item.title = content.name;
+    } else if (content.media_type === undefined && content.name === undefined) {
+      item.title = content.original_title;
+    }
+
+    item.description = content.overview;
+    if (content.backdrop_path !== null) {
+      item.backgroundImage =
+        "https://image.tmdb.org/t/p/original/" + content.backdrop_path;
+    } else {
+      item.backgroundImage = emptyImage;
+    }
+
+    item.vote = content.vote_average;
+    setModalContent(item);
+  };
+
+  if (modal) {
+    document.body.classList.add("active_modal");
+  } else {
+    document.body.classList.remove("active_modal");
+  }
+
   if (data !== undefined) {
     return (
       <div className="gallery">
@@ -96,11 +151,52 @@ const Gallery = ({ urlApi, optionalParameterUrlApi, galleryTitle }) => {
           {data.map((item, key) => (
             <img
               className="images_container__image"
+              onClick={() => {
+                toggleModal();
+                changeContentModal(item);
+              }}
               key={key}
-              src={"https://image.tmdb.org/t/p/original/" + item.poster_path}
+              src={
+                item.poster_path !== null
+                  ? "https://image.tmdb.org/t/p/original/" + item.poster_path
+                  : emptyImage
+              }
             />
           ))}
         </div>
+        {modal && ( //if modal is true, we display the modal
+          <div className="gallery__modal modal">
+            <div className="modal__overlay" onClick={toggleModal}></div>
+            <div className="modal__modal_content modal_content">
+              <img
+                src={modalContent.backgroundImage}
+                className="modal_content__image"
+              />
+              <p className="modal_content__title">{modalContent.title}</p>
+              <div className={"modal_content__vote_container vote_container"}>
+                <span className="vote_container__value">
+                  {modalContent.vote}
+                </span>
+                <FontAwesomeIcon
+                  icon={faStar}
+                  size={"xl"}
+                  color={"yellow"}
+                  className="vote_container__icon"
+                />
+              </div>
+              <p className="modal_content__description">
+                {modalContent.description}
+              </p>
+
+              <button
+                className="modal_content__close_button"
+                onClick={toggleModal}
+              >
+                <FontAwesomeIcon icon={faClose} size={"2xl"} color={"white"} />
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     );
   } else {
